5aa8ea84c7122b6ec5af215707842a98161c2671
Lauren Musante and nikhil batta <nikhil@>
2019-08-20 14:31:51 -0700

    Add if/else statements to make translator work for words that start with either vowels or double-consonant.

--- js/scripts.js
+++ js/scripts.js
@@ -2,16 +2,25 @@ $(document).ready(function(){
   $("#formOne").submit(function(event){
     event.preventDefault();
     // var arr = ["a","e","i","o","u"]
+    var arr1 =[]
     var userInput = $(".inputOne").val();
+    var pusher = arr1.push(userInput)
+    arr1.forEach(function(char){
+    var matcher = char.charAt(0).match(/[aeiou]/gi)
+    var consonantMatcher = char.charAt(0,1).match(/[b-df-hj-np-tv-z]/gi)
+    if (matcher){
+    // var changed =  userInput.toString();
+    var changed1 = userInput.concat("way")
+    $("#formOne").append("<li>" + changed1 + "</li>")
+    }
+    else if (consonantMatcher){
 
-    if (userInput.charAt(0) === "a" || "i" || "o" || "e" || "u"){
-    var changed =  userInput.toString();
-    var changed1 = changed.concat("way")
-      console.log(changed1)
-    }
-    else {
       alert("WOW")
+
+      var consonant = userInput.concat("ay");
+      $("#formOne").append("<li>" + consonant + "</li>")
     }
+   });
   });
 
 });
