20ee8a914f84c64c1b2b724470ec6e8ab3c4ce64
nikhilbatta <nbatta08@gmail.com>
2019-08-21 19:09:27 -0700

    finally fixed the pig latin, 40 lines of code, no array

--- js/scripts.js
+++ js/scripts.js
@@ -4,73 +4,36 @@ $(document).ready(function(){
     var userInput = $(".inputOne").val();
     var startOfLogic = sentenceChecker(userInput);
     $("#formOne").append("<li>" + startOfLogic+ "</li>")
-
   });
 });
-
-// var userInput = $(".inputOne").val();
-
 function sentenceChecker(inputString){
   var words1= "";
   console.log(words1)
   var splitter = inputString.split(" ")
   splitter.forEach(function(split){
-    words1 = inputString + " " + wordChecker(split)
+    words1 = words1 + " " + wordChecker(split)
   });
-
-   return words1;
+  return words1;
 };
-
 function wordChecker(word){
-  // var word = $(".inputOne").val();
+  var matcher1 = (/^[a-zA-Z]+$/).test(word)
+  if (matcher1 === false){
+    alert("omg you have a number in there thats not right")
+    return word;
+  }
   var firstPart = "";
   var secondPart = "";
   var ending = "way"
-  // var matcher1 = (/^[a-zA-Z]+$/).test(word)
-  var matcher1 = (/^[a-zA-Z]+$/).test(word)
-  if (matcher1 === false){
-    console.log("didnt run")
-    return word;
-  }
   for(var i=0; i<word.length; i++){
     var matcher = word[i].match(/[aeiou]/i);
-
-  else if (matcher === null){
+    if (matcher === null){
       firstPart = firstPart.concat(word[i]);
       ending = "ay"
-      // console.log(firstPart + " Is the first part");
     }
     else {
       secondPart = word.substr(i, word.length);
       break;
-      // console.log(secondPart + " Is the second part")
-
     }
   };
-  console.log(secondPart + firstPart + ending);
-
+  return (secondPart + firstPart + ending);
 };
-
-//
-// function characterChecker(word){
-//   // console.log(word + " Is the word");
-//   // var userInput = $(".inputOne").val();
-//   var matcher1 = (/^[a-zA-Z]+$/).test(word)
-//   if (matcher1 === false){
-//     // console.log("didnt work")
-//
-//   }
-//   else{
-//
-//     sentenceChecker(word)
-//   };
-//   // wordChecker(userInput);
-//   // wordChecker(userInput)
-// }
-
-
-// function adder(number1, number2) {
-  //   return number1 + number2;
-  // }
-  //
-  // var result = adder(1,2);
